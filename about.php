75231995ab6e7561da4b7f085125c9b2395a27cf
SiamRdx <siamkhan.cse@gmail.com>
2020-09-28 18:04:43 +0600

    My First Project

---
+++ about.php
@@ -0,0 +1,33 @@
+
+
+    <!-- Navigation -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" align="center"><font color="#3760ae">ONLINE FACULTY REVIEW SYSTEM</font> </h1>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row" style="margin-bottom:50px;margin-left:50px">
+           
+               <P>Online Faculty Review System,
+here we have developed the a faculty feedback system, which is generally used in the university to rate the faculty based on the performance...Here we have 2 modules such as administrator, student.
+
+Administrator is the one who creates the student account by adding all student info and assigning the username and password. 
+Admin also checks the result once all students entered the feedback.
+We can start the demonstration from the login page, where we have given the option to login as admin and student.
+
+You can perform all admin actions such as login to the account and check result.
+
+If you enter the student user and password and select student option, then it will show all of students' information and let you enter the feedback based on the subject.
+
+</P> 
+               
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Footer -->
+     
